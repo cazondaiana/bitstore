5da9ec7ee1f5d2cd14aa43b922835547b80a1c22
Daiana Cazón <86089376+cazondaiana@users.noreply.github.com>
2025-11-05 20:04:19 -0300

    Update tienda.py

--- tienda.py
+++ tienda.py
@@ -240,4 +240,4 @@ if __name__ == "__main__":
     app = crear_aplicacion()
     with app.app_context():
         bd.create_all()
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
